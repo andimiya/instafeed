5b87eb433ae1e46df701c311a4552802021e6c9d
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-17 06:45:15 -1000

    Installed linter, fixed some syntax issues

--- src/app/app.js
+++ src/app/app.js
@@ -1,30 +1,23 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router'
-import { DefaultState, Default Ctrl } from './default'
+import { DefaultState, DefaultCtrl } from './default'
 import { GridServiceName, GridState, GridCtrl, GridService } from './grid'
 
-import '../style/app.css';
+import '../style/app.css'
 
 let app = () => {
   return {
     template: require('./app.html'),
-    controller: 'AppCtrl',
     controllerAs: 'app'
   }
 };
 
-class AppCtrl {
-  constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
-  }
-}
+const MODULE_NAME = 'app'
 
-const MODULE_NAME = 'app';
-
-angular.module(MODULE_NAME, [])
+angular.module(MODULE_NAME, ['ui.router'])
   .directive('app', app)
-  .controller('DefaultCtrl', DefaultCtrl);
-  .controller('GridCtrl', GridCtrl);
+  .controller('DefaultCtrl', DefaultCtrl)
+  .controller('GridCtrl', GridCtrl)
   .service(GridServiceName, GridService)
   .config(($stateProvider) => {
     $stateProvider
