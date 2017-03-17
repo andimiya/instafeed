6a2de036c02199b3521f55a06a2cda567b8800ab
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-17 06:31:41 -1000

    Folder structure for grid

---
+++ src/app/grid/controller.js
@@ -0,0 +1,22 @@
+export const AwwCtrl = ['$scope', 'aww', class AwwCtrl {
+  constructor ($scope, AwwService) {
+    $scope.posts = []
+    this.title = 'Aww Photos'
+    AwwService.getPosts()
+      .then(({
+        status,
+        data: {
+          data: {
+            children
+          }
+        }
+      }) => {
+        if (status !== 200) {
+          $scope.error = `Error fetching /r/aww.json\nStatus: ${status}`
+        }
+        $scope.posts = children
+          .filter(child => child.data.preview)
+          .map(child => child.data)
+      })
+  }
+}]
