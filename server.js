f2efa238d00e5e12476e646ec42b22462d8e6eac
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-16 22:12:22 -1000

    Folder setup for angular

--- server.js
+++ server.js
@@ -5,7 +5,18 @@ const bp = require('body-parser')
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 
+app.get('/api/instafeed')
 
+function getPhotos() {
+  //return new Promise (resolve, reject)
+  //https.get( THE_ENDPOINT)
+    //res.on(data)
+      //store the rawData into a chunk
+    //res.on(end)
+      //resolve(THE_DATA_CHUNK_THING)
+  //if err
+    //reject(REASON_WHY)
+}
 
 app.listen(9000, function() {
   console.log('Listening on 9000');
