3f13771a0a43ce68b83081f80d416972d8b620d7
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-18 08:20:43 -1000

    Saving progress

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express')
 const app = express()
 const bp = require('body-parser')
+const env = require('.env')
+const CLIENT_ID = env.CLIENT_ID
+const ACCESS_TOKEN = env.ACCESS_TOKEN
+
+let endpoint = `https://api.instagram.com/v1/users/${CLIENT_ID}/media/recent/?access_token=${ACCESS_TOKEN}`
 
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 
 app.get('/api/instafeed', (req, res) => {
-  res.send('success')
+  // call the getPhotos function
+  res.send(getPhotos())
 })
+
+// if data.statusCode
+
+function getPhotos () {
+  console.log(endpoint)
+  return endpoint
+  // https.get(endpoint, (data) => {
+  //   data.on('data',)
+  //
+  // })
+  // res.on('data', (chunk) => {
+  //
+  // })
+  // res.on('end', (req, res) => {
+  //   console.log('end')
+  // })
+}
 
 // function getPhotos() {
 //   //return new Promise (resolve, reject)
